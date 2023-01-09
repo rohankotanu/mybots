3caeba08937c5840c4da6183de9e3bed69903f57
Rohan Kota <rohankota@Rohans-MacBook-Pro.local>
2023-01-09 12:35:57 -0600

    created one link

--- generate.py
+++ generate.py
@@ -1,13 +1,5 @@
 import pyrosim.pyrosim as pyrosim
 
-length = 1
-width = 1
-height = 1
-
-x = 0
-y = 0
-z = height/2
-
 pyrosim.Start_SDF("box.sdf")
 
 pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
