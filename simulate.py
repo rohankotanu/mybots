50ab109dcd61ab8b8785d7c2ada265bc5be61dc4
Rohan Kota <rohankota@Rohans-MacBook-Pro.local>
2023-01-09 12:31:11 -0600

    created one link

--- simulate.py
+++ simulate.py
@@ -1,7 +1,15 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+planeId = p.loadURDF("plane.urdf")
+
+p.setGravity(0,0,-9.8)
+
+p.loadSDF("box.sdf")
 
 for i in range(1000):
 	p.stepSimulation()
