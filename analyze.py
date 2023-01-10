c94d19fede01efb8df531beb3336cefb2b8bcace
Rohan Kota <rohankota@Rohans-MacBook-Pro.local>
2023-01-09 23:10:51 -0600

    Open loop control of robot

--- analyze.py
+++ analyze.py
@@ -3,9 +3,13 @@ import matplotlib.pyplot as plt
 
 frontLegSensorValues = np.load('./data/frontLegSensorValues.npy')
 backLegSensorValues = np.load('./data/backLegSensorValues.npy')
+frontLegTargetAngles = np.load('./data/frontLegTargetAngles.npy')
+backLegTargetAngles = np.load('./data/backLegTargetAngles.npy')
 
-plt.plot(frontLegSensorValues,label='Front Leg',linewidth=4)
-plt.plot(backLegSensorValues,label='Back Leg')
+plt.plot(backLegTargetAngles,label='backLeg Target Angles',linewidth=4)
+plt.plot(frontLegTargetAngles,label='frontLeg Target Angles')
+# plt.plot(frontLegSensorValues,label='Front Leg',linewidth=4)
+# plt.plot(backLegSensorValues,label='Back Leg')
 
 plt.legend()
 plt.show()
